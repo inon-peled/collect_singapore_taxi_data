ab9edeb53e006530f85d8e0667d80128b8e30e91
Inon Peled <inon.peled@gmail.com>
2020-10-26 00:54:52 +0100

    WIP

--- remove_indexes.py
+++ remove_indexes.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from tqdm import tqdm
 from glob import glob
 
@@ -7,17 +6,20 @@ import os
 
 
 def remove_index(parquet_path):
-    df = pd.read_parquet(parquet_path)
-    print(parquet_path, len(df))
-    if len(df) > 0:
-        df.to_parquet(os.path.join('no_ts', os.path.basename(parquet_path)), index=False)
+    import pandas as pd
+    try:
+        df = pd.read_parquet(parquet_path)
+        if len(df) > 0:
+            df.to_parquet(os.path.join('no_ts', os.path.basename(parquet_path)), index=False, compression=None, engine='pyarrow')
+    except Exception as e:
+        print(parquet_path, e)
 
 
 def remove_index_from_all_parquets():
-    tasks = glob('./parquet/*/*.parquet')
-#     with Pool(maxtasksperchild=1) as pool:
-    for none_res in tqdm(map(remove_index, tasks), total=len(tasks)):
-        pass
+    tasks = list(filter(lambda f: not os.path.exists('./no_ts/%s' % os.path.basename(f)), glob('./parquet/*/*.parquet')))
+    with Pool(processes=64, maxtasksperchild=1) as pool:
+        for none_res in tqdm(map(remove_index, tasks), total=len(tasks)):
+            pass
 
         
 if __name__ == '__main__':        
